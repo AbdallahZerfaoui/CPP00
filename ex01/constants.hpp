1d99ba97743de5cd1f946f361407cbf33ff05e43
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-05-19 19:21:39 +0200

    Replace macro definitions with constexpr for constants in constants.hpp for improved type safety and clarity

--- ex01/constants.hpp
+++ ex01/constants.hpp
@@ -1,11 +1,11 @@
 #ifndef CONSTANTS_HPP
-# define CONSTANTS_HPP
+#define CONSTANTS_HPP
 
-# define INIT_INDEX 99
-# define MAX_NBR_CONTACTS 8
-# define NBR_INFOS 5
-# define NBR_OPTIONS 3
-# define COLUMN_SIZE 10
-# define DEBUG_MODE 0
+constexpr int INIT_INDEX = 99;
+constexpr int MAX_NBR_CONTACTS = 8;
+constexpr int NBR_INFOS = 5;
+constexpr int NBR_OPTIONS = 3;
+constexpr int COLUMN_SIZE = 10;
+constexpr bool DEBUG_MODE = false;
 
 #endif
