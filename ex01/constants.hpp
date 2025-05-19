8e6eb7ed814c39c688e51789d274d2ca0ed3e107
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-05-19 17:08:10 +0200

    Add constants header file with predefined values for configuration

---
+++ ex01/constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_HPP
+# define CONSTANTS_HPP
+
+# define INIT_INDEX 99
+# define MAX_NBR_CONTACTS 8
+# define NBR_INFOS 5
+# define NBR_OPTIONS 3
+# define COLUMN_SIZE 10
+# define DEBUG_MODE 0
+
+#endif
